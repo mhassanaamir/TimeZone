2ef49ae2adb01cc126b0d5e5633d443277d6d156
Hassan Aamir <hassan.aamir@koderlabs.com>
2024-07-26 14:26:28 +0500

    Add project files.

---
+++ TimeZone/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GeoTimeZone;
+
+string tz = TimeZoneLookup.GetTimeZone(24.87891882071562, 67.08686263125412).Result;
+var test = TimeZoneInfo.FindSystemTimeZoneById(tz);
+Console.WriteLine(DateTime.UtcNow.AddMinutes(test.BaseUtcOffset.TotalMinutes));
